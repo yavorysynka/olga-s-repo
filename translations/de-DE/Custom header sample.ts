4508bcd4afc9dd594c73af6c5454bd65f4bebb65
Olga Yavorska <olha.p.yavorska@oa.edu.ua>
2018-07-13 11:05:24 +0300

    New translations Custom header sample.ts (German)

---
+++ translations/de-DE/Custom header sample.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de" x-crowdin-source-element="translation" sourcelanguage="en">
+  <context>
+    <name>AddBridgeScreen</name>
+    <message>
+      <source>Text</source>
+      <translation type="unfinished">Text for translation</translation>
+    </message>
+    <message>
+      <source>text_2</source>
+      <translation type="unfinished">Text for translation 2</translation>
+    </message>
+  </context>
+</TS>
