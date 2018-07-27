f8cac1c5a1c1e13d20d4f1fc59deb39c4c27dca0
Olga Yavorska <olha.p.yavorska@oa.edu.ua>
2018-07-27 16:02:47 +0300

    New translations sample.php (Afar)

---
+++ translations/aa-ER/sample.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+
+// You can also use comments to leave out parts of a code line
+$x = 5 /* + 15 */ + 5;
+echo $x;
+?>
+
+<?php
+$color = "crwdns1696775:0crwdne1696775:0";
+?>
+
+<?php
+/* en.php - english language file */
+$messages['smth to translate'] = 'crwdns1696776:0crwdne1696776:0';
+$messages['hello'] = 'crwdns1696777:0crwdne1696777:0';
+?>
+
+</body>
+</html>
