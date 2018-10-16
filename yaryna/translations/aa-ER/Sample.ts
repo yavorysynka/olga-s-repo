1424aa56225395f1fe9ed0c2c9e9e35dcdcb41cd
Olga Yavorska <olha.p.yavorska@oa.edu.ua>
2018-10-16 12:55:24 +0300

    New translations Sample.ts (Afar)

---
+++ yaryna/translations/aa-ER/Sample.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="aa" sourcelanguage="en">
+  <context>
+    <name>Page</name>
+    <message>
+      <source>Text for translation</source>
+      <comment>commenting</comment>
+      <translation type="unfinished">crwdns1696766:0crwdne1696766:0</translation>
+    </message>
+    <message>
+      <source>This is some text.</source>
+      <extracomment>some text</extracomment>
+      <translation type="unfinished">crwdns1696767:0crwdne1696767:0</translation>
+    </message>
+  </context>
+  <context>
+    <name>installscript</name>
+    <message>
+      <source>Text for translation #2 </source>
+      <oldcomment>some new comments here</oldcomment>
+      <translation type="unfinished">crwdns1696768:0crwdne1696768:0</translation>
+    </message>
+  </context>
+</TS>
