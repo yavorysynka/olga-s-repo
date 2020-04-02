497a070dcf1a2618d8c90341b9b842a05fd038f9
Olga Yavorska <olga@crowdin.com>
2020-04-02 13:50:12 +0300

    New translations Custom header sample.ts (German)

--- translations/de-DE.ts
+++ translations/de-DE.ts
@@ -1,24 +1,14 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de" sourcelanguage="en">
+<TS version="2.1" language="de" x-crowdin-source-element="translation" sourcelanguage="en">
   <context>
-    <name>Page</name>
+    <name>AddBridgeScreen</name>
     <message>
-      <source>Text for translation</source>
-      <comment>commenting</comment>
+      <source>Text</source>
       <translation type="unfinished"/>
     </message>
     <message>
-      <source>This is some text.</source>
-      <extracomment>some text</extracomment>
-      <translation type="unfinished"/>
-    </message>
-  </context>
-  <context>
-    <name>installscript</name>
-    <message>
-      <source>Text for translation #2 </source>
-      <oldcomment>some new comments here</oldcomment>
+      <source>text_2</source>
       <translation type="unfinished"/>
     </message>
   </context>
