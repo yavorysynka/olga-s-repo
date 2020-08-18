f137a2b4794f67f87813eeacc0ba8b10080791e8
Olga Yavorska <olga@crowdin.com>
2020-08-18 16:32:21 +0300

    New translations Custom header sample.ts (French)

---
+++ translations/fr-FR/Custom header sample.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr" x-crowdin-source-element="translation" sourcelanguage="en">
+  <context>
+    <name>AddBridgeScreen</name>
+    <message>
+      <source>Text</source>
+      <translation>Texte à traduire</translation>
+    </message>
+    <message>
+      <source>text_2</source>
+      <translation>Texte à traduire 2</translation>
+    </message>
+  </context>
+</TS>
